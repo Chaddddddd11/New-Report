8556216680260825d037cc0d3dd319f9722fc9fa
Chaddddddd11 <dcc2ndyrrep.nfjpia2324@gmail.com>
2025-05-26 21:15:35 +0800

    Update auth-check.js

--- auth-check.js
+++ auth-check.js
@@ -1,16 +1,40 @@
 // Get Firebase auth instance
 const auth = firebase.auth();
 
-// Check authentication state
-auth.onAuthStateChanged((user) => {
-    if (!user) {
-        // If user is not logged in, redirect to login page
+// List of pages that don't require authentication
+const PUBLIC_PAGES = ['login.html', 'register.html', 'forgot-password.html'];
+
+// Function to check if current page is public
+function isPublicPage() {
+    const currentPage = window.location.pathname.split('/').pop();
+    return PUBLIC_PAGES.includes(currentPage);
+}
+
+// Function to handle successful authentication
+function handleAuthenticated(user) {
+    // If user is on login/register page but already logged in, redirect to home
+    if (isPublicPage()) {
+        window.location.href = 'home.html';
+    }
+    console.log('User is logged in:', user.uid);
+}
+
+// Function to handle unauthenticated user
+function handleUnauthenticated() {
+    // If not on a public page and not logged in, redirect to login
+    if (!isPublicPage()) {
         // Store the current URL to redirect back after login
         sessionStorage.setItem('redirectAfterLogin', window.location.pathname);
         window.location.href = 'login.html';
+    }
+}
+
+// Check authentication state
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        handleAuthenticated(user);
     } else {
-        // User is logged in, you can access user info if needed
-        console.log('User is logged in:', user.uid);
+        handleUnauthenticated();
     }
 });
 
@@ -23,6 +47,9 @@ function getCurrentUser() {
 function logout() {
     return auth.signOut()
         .then(() => {
+            // Clear any stored data
+            sessionStorage.removeItem('userData');
+            // Redirect to login page
             window.location.href = 'login.html';
         })
         .catch((error) => {
